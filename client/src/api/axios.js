c3096db41300451e567cf91017fab5ea8c0ba1b5
Yatharth Sharma <yatharthsharma3311@gmail.com>
2026-06-27 00:14:20 +0530

    update API URL to production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ai-interview-prep-server-8tog.onrender.com',
+  baseURL: 'https://ai-interview-prep-server-8tog.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
